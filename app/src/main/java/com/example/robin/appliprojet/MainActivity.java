4e01c26690a08b00e57c7ff00e4c434c209daef0
robin <robin_mijeane@hotmail.fr>
2016-11-28 15:11:53 +0100

    menu ok

--- app/src/main/java/com/example/robin/appliprojet/MainActivity.java
+++ app/src/main/java/com/example/robin/appliprojet/MainActivity.java
@@ -80,17 +80,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.recherche) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.achats) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.favoris) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.parametres) {
 
         }
 
